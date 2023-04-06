b8fed279a65bbeef098fc2b67b131eb601371834
cypox <am_neggaz@esi.dz>
2023-04-06 03:06:57 +0200

    added trade details, fixed cost bug

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import pandas as pd
 from datetime import date
 
-tickers = ['MSFT', 'TSLA', 'AAPL', 'NVDA', 'NIO', 'AMD', 'GME', 'AMC', 'NFLX']
+tickers = ['MSFT', 'TSLA', 'AAPL', 'NVDA', 'NIO', 'AMD', 'GME', 'AMC', 'NFLX', 'META']
 
 today = date.today()
 start_date= "2005-01-01"
@@ -31,7 +31,7 @@ class Strategy:
         self.held_stocks = {ticker: 0 for ticker in tickers}
         self.held_stocks_cost = {ticker: 0 for ticker in tickers}
         self.returns_by_stock = {ticker: 0 for ticker in tickers}
-        self.trades = 0
+        self.trades_per_stock = {ticker: 0 for ticker in tickers}
         self.days = 0
     
     def net_worth(self, stock_prices):
@@ -39,12 +39,14 @@ class Strategy:
         net_worth += sum([self.held_stocks[t] * stock_prices[t] for t in tickers])
         print(f"net worth: ${net_worth:0.0f}, liquidity: ${self.balance:0.0f}")
         print(f"estimated returns: ${(net_worth/self.days):.2f} per day")
-        print(f"realised {self.trades} trades")
-        print(f"realised {(self.trades/self.days):.2f} trades per day")
-        print(f"returns by trade: ${(net_worth/self.trades):.2f} per trade")
+        trades = sum(self.trades_per_stock.values())
+        print(f"realised {trades} trades")
+        print(f"realised {(trades/self.days):.2f} trades per day")
+        print(f"returns by trade: ${(net_worth/trades):.2f} per trade")
         print(f"current portfolio: {self.held_stocks}")
         print(f"cost of held stocks: {self.held_stocks_cost}")
         print(f"returns by ticker: {self.returns_by_stock}")
+        print(f"trades by ticker: {self.trades_per_stock}")
 
     def process(self, input_data):
         self.days += 1
@@ -71,7 +73,7 @@ class Strategy:
             actual_qtty = min(qtty, int(self.balance / unit_price))
             if actual_qtty <= 0:
                 return
-            self.trades += 1
+            self.trades_per_stock[ticker] += 1
             self.balance -= unit_price * actual_qtty
             self.held_stocks[ticker] += actual_qtty
             self.held_stocks_cost[ticker] += unit_price * actual_qtty
@@ -81,15 +83,12 @@ class Strategy:
             actual_qtty = min(qtty, self.held_stocks[ticker])
             if actual_qtty <= 0:
                 return
-            self.trades += 1
+            self.trades_per_stock[ticker] += 1
             average_ticker_buy_price = self.held_stocks_cost[ticker] / self.held_stocks[ticker]
-            if average_ticker_buy_price > unit_price:
-                self.returns_by_stock[ticker] -= unit_price * actual_qtty
-            elif average_ticker_buy_price < unit_price:
-                self.returns_by_stock[ticker] += unit_price * actual_qtty
-            self.held_stocks_cost[ticker] -= average_ticker_buy_price * actual_qtty
+            self.returns_by_stock[ticker] += (unit_price - average_ticker_buy_price) * actual_qtty
             self.balance += unit_price * actual_qtty
             self.held_stocks[ticker] -= actual_qtty
+            self.held_stocks_cost[ticker] -= average_ticker_buy_price * actual_qtty
             print(f"sold {actual_qtty} units of {ticker} @ {unit_price} and current balance is ${self.balance}")
 
 bot = Strategy()
